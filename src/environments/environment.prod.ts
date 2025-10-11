aeb6f7b6a8394caa2bc62de92469563705be04ca
Yamilo Peña <y.alberto.pl@gmail.com>
2025-10-11 11:06:34 -0500

    chore(environment): update API and base URLs for new server configuration
    - Updated apiUrl to use new server address http://200.215.229.233:8080/api.
    - Updated apiUrlPublic and baseUrl to match the new host.
    - Removed old references to academical.unadqtc.edu.pe.
    
    These changes align the frontend with the new backend deployment environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://academical.unadqtc.edu.pe:8080/api',
+  apiUrl: 'http://200.215.229.233:8080/api',
   tokenKey: 'OtiProduction@2025.UNADQTC',
-  apiUrlPublic: 'http://academical.unadqtc.edu.pe:8080',
-  baseUrl: 'ttp://academical.unadqtc.edu.pe'
+  apiUrlPublic: 'http://200.215.229.233:8080',
+  baseUrl: 'http://200.215.229.233'
 };
