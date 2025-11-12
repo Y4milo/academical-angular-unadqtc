e581c1f9bebeddc45c8f87df01d05e0bb12b7e63
yamilo <y.alberto.pl@gmail.com>
2025-11-12 04:51:20 -0500

    fix(environment): update production API and base URLs to use HTTPS domain academical.unadqtc.edu.pe - Updated environment.ts for production to point to the secure HTTPS domain. - Replaced old IP and HTTP references with the new HTTPS URLs. - Ensured API endpoints (apiUrl, apiUrlPublic) and baseUrl are consistent. - Improved configuration for deployment on academical.unadqtc.edu.pe.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://200.215.229.233:8080/api',
-  apiUrl: 'http://academical.unadqtc.edu.pe:8080/api',
+  apiUrl: 'https://academical.unadqtc.edu.pe/api',
   tokenKey: 'OtiProduction@2025.UNADQTC',
   // apiUrlPublic: 'http://200.215.229.233:8080',
-  apiUrlPublic: 'http://academical.unadqtc.edu.pe:8080',
+  apiUrlPublic: 'https://academical.unadqtc.edu.pe/api',
   // baseUrl: 'http://200.215.229.233'
-  baseUrl: 'http://academical.unadqtc.edu.pe'
+  baseUrl: 'https://academical.unadqtc.edu.pe'
 };
