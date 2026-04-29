1bf4089ca342a0f53d832469331e207047e1970f
Yamilo Peña <y.alberto.pl@gmail.com>
2026-04-29 00:17:00 -0500

    refactor(login-student): replace `PaymentService` with `UsersService` and `LoginService` for login flow
    - Updated login payload to use `FormData` instead of encoded array.
    - Refactored login logic to remove payment validation and integrate user role-based redirection.
    - Enhanced `NotificationService` usage for improved feedback during login.

--- src/app/dashboard/student/login-student/login-student.component.ts
+++ src/app/dashboard/student/login-student/login-student.component.ts
@@ -2,10 +2,13 @@ import {Component, contentChild} from '@angular/core';
 import {LoginBaseComponent} from '../../login-base/login-base.component';
 import {NotificationService} from '../../../services/notification.service';
 import {Router} from '@angular/router';
-import {PaymentService} from '../../../services/payment.service';
 import {encodeArray} from '../../../helper/helper.util';
 import {NOTIFICATION_MESSAGE} from '../../../core/constants/notification_message';
 import {STATUS} from '../../../core/constants/status';
+import {LoginService} from '../../../services/login.service';
+import {UsersService} from '../../../services/users.service';
+import {home_link, HomeKey} from '../../../core/constants/home_link';
+import {PATHS} from '../../../core/constants/paths';
 
 @Component({
   selector: 'app-login-page',
@@ -27,33 +30,34 @@ export class LoginStudentComponent {
   password: string = '';
 
   constructor(
-    private notification: NotificationService,
+    private notificationService: NotificationService,
     private router: Router,
-    private paymentService: PaymentService,
+    private usersService: UsersService,
+    private loginService: LoginService,
   ) {}
 
   login() {
-    const payload = {
-      code_student: this.user,
-      password: this.password
-    };
-    const loginData = encodeArray(payload);
+    const loginData = new FormData();
+    loginData.append('nick', this.user);
+    loginData.append('password', this.password);
 
-    this.paymentService.validatePayment(loginData).subscribe({
-      next: (paymentRes) => {
-        // if (paymentRes.status === STATUS.success) {
-        //   sessionStorage.setItem('payment_id', paymentRes.payload.data);
-        //   // ✅ Guardar datos en la sesión
-        //   this.notification.success(paymentRes.payload.title, paymentRes.payload.message);
-        //   setTimeout(() => {
-        //     this.router.navigate(['/student-card-registration']);
-        //   }, 2000);
-        // } else if (paymentRes.status === 'warning') {
-        //   this.notification.warning(paymentRes.payload.title, paymentRes.payload.message);
-        // }
+    this.usersService.logIn(loginData).subscribe({
+      next: (loginUserData) => {
+        if (loginUserData.status === STATUS.success) {
+          const staff_user = this.loginService.setUser(loginUserData);
+          const key = staff_user.role.value as HomeKey;
+          let route = home_link[key];
+          if (route === undefined)
+            route = PATHS.login.student;
+            console.log('route: ' + route);
+            // setTimeout(() => {
+            //   this.router.navigate([route]);
+            // }, 2000);
+        }
+        this.notificationService.notifyApiData(loginUserData);
       },
       error: (error) => {
-        this.notification.error(
+        this.notificationService.error(
           NOTIFICATION_MESSAGE.error_connection.title,
           NOTIFICATION_MESSAGE.error_connection.message
         );
