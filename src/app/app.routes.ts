94e6adecf023db4f92a86f1613d19fe97eda717d
Yamilo Peña <y.alberto.pl@gmail.com>
2026-08-08 17:30:42 -0500

    Revert "Add admin event certificates interface"
    This reverts commit cfff02493c9213494df5c69b37a1ac1cb96395af.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -35,9 +35,6 @@ import {
 import {StaffMenuBaseComponent} from './dashboard/staff-menu-base/staff-menu-base.component';
 import {AuthRoleGuard} from './guards/auth-role.guard';
 import {AuthStudentGuard} from './guards/auth-student.guard';
-import {
-  EventCertificatesAdminComponent
-} from './dashboard/staff/events/event-certificates-admin/event-certificates-admin.component';
 
 export const routes: Routes = [
   {
@@ -87,10 +84,6 @@ export const routes: Routes = [
       {
         path: PATHS.academic.student.approvedAverage.path,
         component: ApprovedAverageComponent
-      },
-      {
-        path: PATHS.admin.events.certificates.path,
-        component: EventCertificatesAdminComponent
       },
     ]
   },
