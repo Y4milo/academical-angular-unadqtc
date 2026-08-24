8ee8d7b181b9045146fe10d8c29f7a2960bdca02
Yamilo <y.alberto.pl@gmail.com>
2026-08-24 12:55:11 -0500

    feat: add semester selector to student card management

--- src/app/models/dictionary.model.ts
+++ src/app/models/dictionary.model.ts
@@ -7,6 +7,7 @@ export interface Dictionary {
   value?: string;
   label: string;
   json?: Record<string, any>;
+  is_current?: boolean;
   student_card_registration?: {
     can_register: boolean;
     start_date?: string | null;
